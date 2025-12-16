27e2c8503914706f1251b924efd822d0355cb25f
hankyeol1 <chohankyeol0519@gmail.com>
2025-12-17 03:33:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ HIDE_Press_Enter_to_apply = """
 }
 
 div.stButton > button {
-    background-color: #FFB746;
+    background-color: #FFA526;
     color: white;
     border-radius: 10px;
     border: none;
